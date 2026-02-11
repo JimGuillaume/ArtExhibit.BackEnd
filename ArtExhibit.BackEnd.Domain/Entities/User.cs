315f13ec5e4027c9a3f15ce0b1ba282331d28487
JimGuillaume <jimguillaume99@gmail.com>
2026-02-11 15:07:24 +0100

    New Entity User

---
+++ ArtExhibit.BackEnd.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtExhibit.BackEnd.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public UserType UserType { get; set; } = null!;
+}
