eced0f83dffc473ab15389a569b5570d54181ce9
Jim Guillaume <jimguillaume99@gmail.com>
2026-02-26 17:13:16 +0100

    Corrected Implementation error in DataSet

---
+++ ArtExhibit.BackEnd.Infrastructure/Data/Migrations/20260226154302_ReportSampleDataMissing.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtExhibit.BackEnd.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReportSampleDataMissing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Items_ItemId",
+                table: "Reports");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reports_Users_UserId",
+                table: "Reports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_ItemId",
+                table: "Reports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reports_UserId",
+                table: "Reports");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_ItemId",
+                table: "Reports",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_UserId",
+                table: "Reports",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Items_ItemId",
+                table: "Reports",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Users_UserId",
+                table: "Reports",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
